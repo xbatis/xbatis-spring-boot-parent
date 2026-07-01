21a6e5a9c2b5f7ab4a83464134dac8072e0d8816
Ai东 <abc-127@live.cn>
2026-07-01 14:45:38 +0800

    1.10.6

--- xbatis-spring-boot-starter/src/main/java/org/mybatis/spring/boot/autoconfigure/XbatisDDLAutoRegistrar.java
+++ xbatis-spring-boot-starter/src/main/java/org/mybatis/spring/boot/autoconfigure/XbatisDDLAutoRegistrar.java
@@ -83,10 +83,9 @@ public class XbatisDDLAutoRegistrar implements ImportBeanDefinitionRegistrar {
         genericBeanDefinition.setBeanClass(XbatisDDLAuto.class);
         genericBeanDefinition.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
         genericBeanDefinition.getPropertyValues().add("entities", entities);
-        genericBeanDefinition.getPropertyValues().add("mode", annoAttrs.get(LambdaUtil.getName(XbatisDDLAutoScan::mode)));
+        genericBeanDefinition.getPropertyValues().add("mode", mode);
         genericBeanDefinition.getPropertyValues().add("dbType", annoAttrs.get(LambdaUtil.getName(XbatisDDLAutoScan::dbType)));
         genericBeanDefinition.getPropertyValues().add("dataSource", annoAttrs.get(LambdaUtil.getName(XbatisDDLAutoScan::dataSource)));
-        genericBeanDefinition.setAttribute("order", "0");
         genericBeanDefinition.setLazyInit(false);
 
         registry.registerBeanDefinition("xbatisDDLAuto" + (index == 0 ? "" : index), genericBeanDefinition);
